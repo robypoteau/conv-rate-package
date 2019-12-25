2438c61d0a1b7102b9f19612d6e4fe227ad0a980
Roby Poteau <robypoteau@gmail.com>
2019-12-25 10:16:17 -0500

    Edit README and setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(name="convrate"
+with open('README.md') as f:
+    README = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    author="Roby Poteau"
+    ,author_email="robypoteau@gmail.com"
+    ,name="convrate"
     ,version='1.0'
+    ,long_description=README
+    ,url='https://github.com/robypoteau/conv-rate-package'
     ,packages=find_packages()
     ,license='MIT'
     ,setup_requires=['pytest-runner']
